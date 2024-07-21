659817ed558b3254c164d583c60fa88efa414cc7
Nobres-gui <guilherme.souza@sptech.school>
2024-07-20 21:10:58 -0300

    Finalização do projeto AluraMidi

--- Front-End/Javascript/AluraMidi/js/main.js
+++ Front-End/Javascript/AluraMidi/js/main.js
@@ -1,8 +1,59 @@
-const listaTeclas = document.querySelectorAll('.tecla');
 
-function tocarSom() {
-    document.querySelector('#som_tecla_pom').play();
-
+function tocarSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+    
+    if (elemento === null || elemento.localName != 'audio') {
+        console.log('Elemento não encontrado')
+    } else {
+        elemento.play();
+    }
 }
 
-listaTeclas[0].onclick = tocarSomPom;
+const listaTeclas = document.querySelectorAll('.tecla');
+
+for (let i = 0; i < listaTeclas.length; i++) {
+    const tecla = listaTeclas[i];
+    const idElementoAudio = tecla.classList[1];
+    const idAudio = `#som_${idElementoAudio}`
+
+    console.log(idElementoAudio)
+
+    tecla.onclick = function () {
+        tocarSom(idAudio);
+    };
+
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+        console.log(evento.code);
+    }
+    tecla.onkeyup = function (evento) {
+            tecla.classList.remove('ativa');
+    }
+}
+
+
+
+
+
+
+
+// Usando while como ferramenta de repetição
+
+// let contador = 0;
+
+// while (contador < listaTeclas.length) {
+//     const tecla = listaTeclas[contador];
+//     const idElementoAudio = tecla.classList[1];
+//     const idAudio = `#som_${idElementoAudio}`
+
+//     console.log(idElementoAudio)
+
+//     tecla.onclick = function () {
+//         tocarSom(idAudio);
+//     };
+
+//     contador++
+//     console.log(idAudio);
+// }

1f41dc658386facd4eb75a97a0a54a4365b7a0d1
Nobres-gui <guilherme.souza@sptech.school>
2025-02-05 11:43:03 -0300

    Curso de iniciação sobre API

---
+++ Back-End/C#/Orientação a objeto/screenSound/screenSound/Program.cs
@@ -0,0 +1,57 @@
+﻿string mensagemBoasVindas = "Boas vindas ao Screen Sound";
+
+void exibirMensagemBoasVindas()
+{
+    Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+    Console.WriteLine(mensagemBoasVindas);
+}
+
+void exibirOpcoesMenu()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostras todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite 0 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+
+    // ! indica que o  valor inserido não pode ser nulo
+    string opcaoEscolhida = Console.ReadLine()!;
+    // int.Parse transforma uma string em numero;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1: 
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 2: 
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 3:
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 4: 
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 5:
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case -1:
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+    }
+
+}
+
+exibirMensagemBoasVindas();
+exibirOpcoesMenu();
---
+++ Back-End/JavaScript/Consumindo API/VidFlow/script.js
@@ -0,0 +1,76 @@
+const containerVideos = document.querySelector(".videos__container");
+
+async function buscaMostrarVideos() {
+    //"buscas os dados dentro da api"
+    const busca = await fetch("http://localhost:3000/videos");
+    const videos = await busca.json();
+    try {
+        videos.forEach(videos => {
+            //"erros personalizados"
+            if (videos.categoria == "") {
+                throw new Error("Vídeo não tem categoria")
+            }
+            containerVideos.innerHTML += `
+            <li class="videos__item">
+                <iframe src="${videos.url}" title="${videos.titulo}" frameborder="0" allowfullscreen></iframe>
+                <div class="descricao-video">
+                    <img class="img-canal" src="${videos.imagem}" alt="Logo do canal">
+                    <h3 class="titulo-video">${videos.titulo}</h3>
+                    <p class="titulo-canal">${videos.descricao}</p>
+                    <p class="categoria" hidden>${videos.categoria}</p>
+                </div>
+            </li>
+            `;
+        })
+    } catch (error) {
+        containerVideos.innerHTML = `
+        <p> Houve um erro ao carregar os vídeos: ${error}</p>`;
+    }
+}
+buscaMostrarVideos();
+
+const barraPesquisa = document.querySelector(".pesquisar__input");
+
+barraPesquisa.addEventListener("input", filtrarPesquisa);
+
+function filtrarPesquisa() {
+    const videos = document.querySelectorAll(".videos__item");
+
+    if (barraPesquisa.value != "") {
+        //faz o for passar por todos os videos
+        for (let video of videos) {
+            let titulo = video.querySelector(".titulo-video").textContent.toLowerCase();
+            let valorFiltro = barraPesquisa.value.toLowerCase();
+
+            if (!titulo.includes(valorFiltro)) {
+                video.style.display = "none";
+            } else {
+                video.style.display = "block";
+            }
+        }
+    } else {
+        video.style.display = "block";
+    }
+}
+
+
+const botaoCategoria = document.querySelectorAll(".superior__item");
+
+botaoCategoria.forEach((botao) => {
+    let nomeCategoria = botao.getAttribute("name");
+    botao.addEventListener("click", () => filtrarCategoria(nomeCategoria));
+})
+
+function filtrarCategoria(filtro){
+    const videos = document.querySelectorAll(".videos__item");
+    for(let video of videos){
+        let categoria = video.querySelector(".categoria").textContent.toLowerCase();
+        let valorFiltro = filtro.toLowerCase();
+
+        if (!categoria.includes(valorFiltro) && valorFiltro != 'tudo') {
+            video.style.display ="none";
+        } else {
+            video.style.display ="block";
+        }
+    }
+}
